def3f0b4638a54a33b473bf323d7815927fcb431
effozen <ashgrayblue0@gmail.com>
2025-01-31 01:36:12 +0900

    fix : fix index.js path error

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import forbiddenImports from "./rules/fsd-forbidden-imports.js";
-import noRelativeImports from "./rules/fsd-no-relative-imports.js";
-import noPublicApiSidestep from "./rules/fsd-no-public-api-sidestep.js";
-import noCrossSliceDependency from "./rules/fsd-no-cross-slice-dependency.js";
-import noUiInBusinessLogic from "./rules/fsd-no-ui-in-business-logic.js";
-import noGlobalStoreImports from "./rules/fsd-no-global-store-imports.js";
-import orderedImports from "./rules/fsd-ordered-imports.js";
+import forbiddenImports from "./rules/forbidden-imports.js";
+import noRelativeImports from "./rules/no-relative-imports.js";
+import noPublicApiSidestep from "./rules/no-public-api-sidestep.js";
+import noCrossSliceDependency from "./rules/no-cross-slice-dependency.js";
+import noUiInBusinessLogic from "./rules/no-ui-in-business-logic.js";
+import noGlobalStoreImports from "./rules/no-global-store-imports.js";
+import orderedImports from "./rules/ordered-imports.js";
 
 export default {
   rules: {
